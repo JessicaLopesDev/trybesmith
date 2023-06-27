fae6ab26252f89f892ac4b43b984c3107c87afd2
Jessica Lopes <jessicalopes.dev@gmail.com>
2023-06-27 18:01:55 -0300

    Rota post products ok

--- tests/integration/products/create.test.ts
+++ tests/integration/products/create.test.ts
@@ -1,10 +1,27 @@
 import sinon from 'sinon';
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
+import app from '../../../src/app';
+import ProductModel from '../../../src/database/models/product.model';
+import productMock from '../../mocks/product.mock';
 
 chai.use(chaiHttp);
 
-describe('POST /products', function () { 
-  beforeEach(function () { sinon.restore(); });
+describe('POST /products', function () {
+  beforeEach(function () {
+    sinon.restore();
+  });
+  it('Deve devolver um erro ao enviar um email inexistente no banco', async function () {
+    const httpRequestBody = productMock.validReq;
 
+    const mockReturn = ProductModel.build(productMock.validRes);
+    sinon.stub(ProductModel, 'create').resolves(mockReturn);
+    const response = await chai
+      .request(app)
+      .post('/products')
+      .send(httpRequestBody);
+
+    expect(response.body).to.be.equal(productMock.validRes);
+    expect(response.status).to.eq(201);
+  });
 });
---
+++ tests/mocks/product.mock.ts
@@ -0,0 +1,14 @@
+const validReq = {
+  name: 'Martelo de Thor',
+  price: '30 peças de ouro',
+  orderId: 4,
+};
+
+const validRes = {
+  id: 7,
+  name: 'Martelo de Thor',
+  price: '30 peças de ouro',
+  orderId: 4,
+};
+
+export default { validReq, validRes };
